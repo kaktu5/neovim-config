abcbe34bff9a3457f08071d55e0f48fb5f263960
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-10-05 16:22:21 +0200

    neovim/lsp: init

---
+++ neovim/lsp/languages.nix
@@ -0,0 +1,34 @@
+_: {
+  vim.languages = {
+    enableDAP = true;
+    enableExtraDiagnostics = true;
+    enableFormat = true;
+    enableTreesitter = true;
+
+    clang.enable = true;
+    css = {
+      enable = true;
+      format.type = "biome";
+    };
+    haskell.enable = true;
+    html.enable = true;
+    markdown = {
+      enable = true;
+      format.type = "prettierd";
+    };
+    nix = {
+      enable = true;
+      lsp.server = "nixd";
+      extraDiagnostics.enable = false;
+    };
+    ocaml.enable = true;
+    rust = {
+      enable = true;
+      crates.enable = true;
+    };
+    typst = {
+      enable = true;
+      format.type = "typstyle";
+    };
+  };
+}
---
+++ neovim/lsp/lsp.nix
@@ -0,0 +1,6 @@
+_: {
+  vim.lsp = {
+    enable = true;
+    formatOnSave = true;
+  };
+}
