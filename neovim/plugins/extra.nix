1276ef020b3c667b5207b4f30f71295e1f9d6cf1
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-10-07 21:47:20 +0200

    neovim/plugins/extra: remove vim-startuptime

--- neovim/plugins/extra.nix
+++ neovim/plugins/extra.nix
@@ -11,7 +11,6 @@
 
     extraPlugins = with pkgs.vimPlugins; {
       vim-sort-motion.package = vim-sort-motion;
-      vim-startuptime.package = vim-startuptime;
       vim-tmux-navigator.package = vim-tmux-navigator;
     };
   };
