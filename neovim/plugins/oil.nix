e6505c9040c21b5c810c35a2f79b6d0c033e0476
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-10-08 19:02:19 +0200

    neovim/plugins/oil: use special toLuaObject syntax

--- neovim/plugins/oil.nix
+++ neovim/plugins/oil.nix
@@ -1,35 +1,48 @@
-{lib, ...}: let
-  inherit (lib.generators) mkLuaInline;
+_: let
   border = ["┌" "─" "┐" "│" "┘" "─" "└" "│"];
 in {
   vim = {
     utility.oil-nvim = {
       enable = true;
       setupOpts = {
-        columns = mkLuaInline ''
-          {
-            "permissions",
-            "size",
-            "mtime",
-            { "icon", add_padding = false },
-          }
-        '';
+        columns = {
+          "@0" = "permissions";
+          "@1" = "size";
+          "@2" = "mtime";
+          "@3" = {
+            "@0" = "icon";
+            add_padding = false;
+          };
+        };
         skip_confirm_for_simple_edits = true;
         constrain_cursor = "name";
         watch_for_changes = true;
         use_default_keymaps = false;
-        keymaps = mkLuaInline ''
-          {
-            ["<CR>"] = "actions.select",
-            ["<C-p>"] = "actions.preview",
-            ["<C-c>"] = { "actions.close", mode = "n" },
-            ["-"] = { "actions.parent", mode = "n" },
-            ["_"] = { "actions.open_cwd", mode = "n" },
-            ["gs"] = { "actions.change_sort", mode = "n" },
-            ["gx"] = "actions.open_external",
-            ["g."] = { "actions.toggle_hidden", mode = "n" },
-          }
-        '';
+        keymaps = {
+          "<CR>" = "actions.select";
+          "<C-p>" = "actions.preview";
+          "<C-c>" = {
+            "@0" = "actions.close";
+            mode = "n";
+          };
+          "-" = {
+            "@0" = "actions.parent";
+            mode = "n";
+          };
+          "_" = {
+            "@0" = "actions.open_cwd";
+            mode = "n";
+          };
+          "gs" = {
+            "@0" = "actions.change_sort";
+            mode = "n";
+          };
+          "gx" = "actions.open_external";
+          "g." = {
+            "@0" = "actions.toggle_hidden";
+            mode = "n";
+          };
+        };
         view_options = {
           show_hidden = true;
           case_insensitive = true;
