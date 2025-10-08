fc6b863de376dd9d6c7b46b7d2fb1e8a47789b26
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-10-08 19:02:19 +0200

    neovim/lsp/diagnostics: fix icons

--- neovim/lsp/diagnostics.nix
+++ neovim/lsp/diagnostics.nix
@@ -1,15 +1,18 @@
 {lib, ...}: let
-  inherit (lib.nvim.lua) toLuaObject;
+  inherit (lib.generators) mkLuaInline;
 in {
   vim = {
     diagnostics = {
       enable = true;
-      config.signs.text = toLuaObject {
-        "vim.diagnostic.severity.ERROR" = " ";
-        "vim.diagnostic.severity.WARN" = " ";
-        "vim.diagnostic.severity.INFO" = " ";
-        "vim.diagnostic.severity.HINT" = " ";
-      };
+      nvim-lint.enable = true;
+      config.signs.text = mkLuaInline ''
+        {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.HINT] = " ",
+        }
+      '';
     };
 
     maps.normal.gK = {
